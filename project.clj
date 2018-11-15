63fae9cc340f513bdb7eca9f10716a02929188cb
Morgan Astra <m@morganastra.me>
2018-11-15 00:50:47 -0800

    Put ring-devel back in main dependencies
    It's needed in prod for the ring-stacktrace middleware

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :description "Pronoun.is is a website for personal pronoun usage examples"
   :url "https://pronoun.is"
   :license "GNU Affero General Public License 3.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+  :dependencies [[compojure "1.6.1"]
                  [environ "1.1.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [org.clojure/clojure "1.9.0"]
+                 [ring/ring-devel "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]
             [lein-ring "0.9.7"]]
@@ -14,6 +15,5 @@
   :uberjar-name "pronouns-standalone.jar"
   ;; FIXME morgan.astra <2018-11-14 Wed>
   ;; Is this production profile used for anything?
-  :profiles {:production {:env {:production true}}
-             :test {:dependencies [[ring/ring-devel "1.7.1"]]}}
+  :profiles {:production {:env {:production true}}}
   :ring {:handler pronouns.web/app})
