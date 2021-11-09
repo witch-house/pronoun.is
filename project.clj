2e248f830e09f576cd41162078c770d2bc187e51
Randall Mason <ranmason@paypal.com>
2021-11-09 19:21:20 +0000

    Update dependencies fixing lein 2.9.0 issue
    * Run `lein ancient` and update to the latest
    * Add `core.unify` to fix bug with lein 2.9.0:
      * https://github.com/oakes/Nightlight/issues/53

--- project.clj
+++ project.clj
@@ -2,16 +2,18 @@
   :description "Pronoun.is is a website for personal pronoun usage examples"
   :url "https://pronoun.is"
   :license "GNU Affero General Public License 3.0"
-  :dependencies [[compojure "1.6.1"]
-                 [environ "1.1.0"]
+  :dependencies [[compojure "1.6.2"]
+                 [environ "1.2.0"]
                  [hiccup "1.0.5"]
                  [lambdaisland/ring.middleware.logger "0.5.1"]
-                 [org.clojure/clojure "1.9.0"]
-                 [ring/ring-devel "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [org.clojure/clojure "1.10.3"]
+                 [ring/ring-devel "1.9.4"]
+                 [ring/ring-jetty-adapter "1.9.4"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]
-            [lein-ring "0.9.7"]]
+  :plugins [[environ/environ.lein "0.2.1" :hooks false]
+            [lein-ring "0.9.7"]
+            [lein-ancient "1.0.0-RC3"]
+            [org.clojure/core.unify "0.5.7"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "pronouns-standalone.jar"
   ;; FIXME morgan.astra <2018-11-14 Wed>
